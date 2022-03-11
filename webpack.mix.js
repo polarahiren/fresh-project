74dffc10e34596fbd8d7606aeebb01c5fa4223ff
polara Hiren <hiren.polara22@gmail.com>
2022-03-12 00:15:28 +0530

    webpack file change

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,8 @@ const mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'public/js')
     .sass('src/scss/app.scss', 'public/css')
+    .copy('src/fonts', 'public/fonts')
+
+    mix.options({
+        processCssUrls: false
+    }); 
